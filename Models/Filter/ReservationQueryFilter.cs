2012c0c6059efda682518eaf9bbeaa21fe47cc94
Amjad <amjadbkhour@gmail.com>
2026-09-21 22:25:36 +0300

    create a querybody

---
+++ Models/Filter/ReservationQueryFilter.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyHubAPI.Models.Filter
+{
+    public class ReservationQueryFilter
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+        public byte? ReservationStatus { get; set; }
+        public DateTime? StartTime { get; set; }
+
+    }
+}
